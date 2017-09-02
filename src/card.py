e808af492fc77559ede8ca553cbb62f3ec2aa5e0
FreeMasen <RFM@Herman.local>
2017-09-01 19:09:27 -0500

    remove private _ from value and suit

--- src/card.py
+++ src/card.py
@@ -2,23 +2,23 @@ suits = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
 
 class Card():
     def __init__(self, suit, number):
-        self._value = number
-        self._suit = suit
+        self.value = number
+        self.suit = suit
     
     def get_value(self):
-        if (self._value == 0):
+        if (self.value == 0):
             return 'A'
-        if (self._value < 10):
-            return str(self._value + 1)
-        if (self._value == 10):
+        if (self.value < 10):
+            return str(self.value + 1)
+        if (self.value == 10):
             return 'J'
-        if (self._value == 11):
+        if (self.value == 11):
             return 'Q'
-        if (self._value == 12):
+        if (self.value == 12):
             return 'K'
 
     def get_suit(self):
-        return suits[self._suit]
+        return suits[self.suit]
 
     def __str__(self):
         return '%s %s' % (self.get_suit(), self.get_value())
