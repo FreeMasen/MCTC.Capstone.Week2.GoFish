175dcca49555334ce4532bb3511de0564c908959
FreeMasen <RFM@Herman.local>
2017-09-01 19:09:06 -0500

    move deck to set

--- src/deck.py
+++ src/deck.py
@@ -4,10 +4,10 @@ from src.card import Card
 
 class Deck():
     def __init__(self):
-        self.cards = list()
+        self.cards = set()
         for i in range(4):
             for j in range(13):
-                self.cards.append(Card(i, j))
+                self.cards.add(Card(i, j))
     
     def get_card(self):
         index = randint(0, len(self.cards) - 1)
