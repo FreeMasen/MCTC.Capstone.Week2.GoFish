bea341de434e66b54c7ae20ba3f55ba2be93648e
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-09-05 23:26:37 -0500

    Added code to Game. Basic functions to get cards for a hand and as for a face value. Also changed cards Set() to a list.

--- src/deck.py
+++ src/deck.py
@@ -4,11 +4,18 @@ from src.card import Card
 class Deck():
     '''Deck of cards, initialized with a standard 52 cards'''
     def __init__(self):
-        self.cards = set()
+        self.cards = list()
         for i in range(4):
             for j in range(13):
-                self.cards.add(Card(i, j))
+                self.cards.append(Card(i, j))
     def get_card(self):
         '''Get a card from a random position in the deck'''
         index = randint(0, len(self.cards) - 1)
         return self.cards.pop(index)
+
+    def __str__(self):
+        return '%s' % (self.get_card())
+    def __repr__(self):
+        return self.__str__()
+
+
