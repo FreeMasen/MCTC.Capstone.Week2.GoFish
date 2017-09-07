1e589dae95fa5ae8f7ea18ada3650fd1f93936da
Robert Masen <rmasen@USIHQ.net>
2017-09-07 08:07:13 -0500

    white space

--- src/deck.py
+++ src/deck.py
@@ -12,10 +12,7 @@ class Deck():
         '''Get a card from a random position in the deck'''
         index = randint(0, len(self.cards) - 1)
         return self.cards.pop(index)
-
     def __str__(self):
         return '%s' % (self.get_card())
     def __repr__(self):
         return self.__str__()
-
-
