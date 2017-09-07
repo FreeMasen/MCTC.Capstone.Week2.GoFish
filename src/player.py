4265105fa9f5180978751f4e2b58bafe3cbaf06a
FreeMasen <RFM@Herman.local>
2017-09-06 20:33:35 -0500

    Move to a hand/book focused model, implement check_for_books and give_cards

--- src/player.py
+++ src/player.py
@@ -1,33 +1,38 @@
-from src.card import Card
-from src.deck import Deck
-import random
+'''Player in the game GoFish'''
+from src.book import Book
 
 class Player():
-
+    '''A player in the game GoFish'''
     def __init__(self):
-        self.playerHand = []
-
-    def draw(self, deck):
-        self.playerHand.append(deck.get_card(self))
-        return self
-
-
-class Bot():
-
-    def __init__(self):
-        self.botHand = []
-
-    def draw(self, deck):
-        self.botHand.append(deck.get_card(self))
-        return self
-
-
-#initial hand class
-class Bot:
-
-    botHand = []
-
-    while len(botHand) != 7:
-        botHand.append(random.randint(1,11)) #how do I implement Cards class here?
-    if len(botHand) == 7:
-        print("Bot has", botHand)
+        self.hand = list()
+        self.books = list()
+    def accept_card(self, card):
+        '''accept a card from the deck or opponent, this checks for any valid books'''
+        try:
+            last_matching = list(map(lambda card: card.value, self.hand)).index(card.value)
+            self.hand.insert(last_matching + 1, card)
+        except ValueError:
+            self.hand.append(card)
+        self.check_for_books()
+    def check_for_books(self):
+        '''check the current hand for any possible books'''
+        possible_books = dict()
+        while len(self.hand) > 0:
+            card = self.hand.pop()
+            try:
+                book = possible_books[card.value]
+                book.add(card)
+            except KeyError:
+                new_book = Book()
+                new_book.add(card)
+        complete_books = filter(lambda book: book.is_complete(),\
+        list(possible_books.values()))
+        self.books.extend(complete_books)
+        incomplete_books = filter(lambda book: not book.is_complete(), \
+        list(possible_books.values()))
+        self.hand = list(map(lambda book: book.cards, incomplete_books))
+    def give_cards(self, value):
+        '''Return any cards with a matching value and remove them from the player's hand'''
+        ret = list(filter(lambda card: card.value == value, self.hand))
+        self.hand = list(filter(lambda card: card.value != value, self.hand))
+        return ret
